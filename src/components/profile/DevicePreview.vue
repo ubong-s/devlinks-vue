bad33c36e8c7e54437033e67b2adc2af6fb53534
Ubong Sylvester <ubongsly@gmail.com>
2023-09-24 15:20:37 +0100

    refactored code

--- src/components/profile/DevicePreview.vue
+++ src/components/profile/DevicePreview.vue
@@ -4,7 +4,8 @@
     class="hidden rounded-lg bg-white lg:flex items-center justify-center h-screen p-16 xl:w-[500px] 2xl:w-[590px] sticky top-2"
   >
     <div class="relative h-[500px] overflow-hidden">
-      <div class="absolute left-0 top-4 bottom-8 w-full py-8 px-7 overflow-hidden">
+      <div class="absolute bg-white left-4 right-4 top-8 bottom-7 rounded-xl"></div>
+      <div class="absolute left-0 top-4 bottom-7 w-full py-8 px-7 overflow-hidden">
         <!-- Profile Image -->
         <div
           class="rounded-full h-[75px] w-[75px] mx-auto overflow-hidden border-2 border-primary-blue mb-4"
@@ -15,20 +16,20 @@
             alt="sample"
             class="object-center"
           />
-          <img else src="/assets/sample.png" alt="sample" class="object-center" />
+          <img else src="/assets/headshot.png" alt="sample" class="object-center" />
         </div>
 
         <!-- Profile Name and Email -->
         <div class="text-center mb-9">
           <h1 class="font-bold mb-0 w-full bg-white">
-            {{ currentUser.firstName }} {{ currentUser.lastName }}
+            {{ profileDetails.firstName }} {{ profileDetails.lastName }}
           </h1>
-          <p class="text-gray-600 text-sm w-full bg-white">{{ currentUser.workEmail }}</p>
+          <p class="text-gray-600 text-sm w-full bg-white">{{ profileDetails.workEmail }}</p>
         </div>
 
         <!-- Profile Links -->
         <ul class="flex flex-col gap-3">
-          <li v-for="link in currentUser.links" :key="link.id">
+          <li v-for="link in links" :key="link.id">
             <div
               class="px-2 py-2 border-2 w-full text-sm text-white rounded-lg flex justify-between items-center"
               :class="buttonVariants[link.code]"
@@ -68,7 +69,7 @@ export default {
     };
   },
   computed: {
-    ...mapWritableState(useUserStore, ['currentUser'])
+    ...mapWritableState(useUserStore, ['currentUser', 'links', 'profileDetails'])
   },
 
   components: {
